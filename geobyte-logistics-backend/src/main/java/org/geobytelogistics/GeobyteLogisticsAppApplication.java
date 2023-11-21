dd0443afa6c7d0dbfdac866e3276c37698eaa8f4
Usani001 <emmanuel.usani@decagon.dev>
2023-11-21 07:07:56 +0100

    Handled Cors

--- geobyte-logistics-backend/src/main/java/org/geobytelogistics/GeobyteLogisticsAppApplication.java
+++ geobyte-logistics-backend/src/main/java/org/geobytelogistics/GeobyteLogisticsAppApplication.java
@@ -2,8 +2,10 @@ package org.geobytelogistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.geobytelogistics")
 public class GeobyteLogisticsAppApplication {
 
     public static void main(String[] args) {
